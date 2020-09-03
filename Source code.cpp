7e118c60e400d027bf05bf2a55054edb95214e79
kb-1024bytes <70023044+kb-1024bytes@users.noreply.github.com>
2020-09-03 13:02:56 +0530

    Update Source code.cpp

--- Source code.cpp
+++ Source code.cpp
@@ -3,107 +3,69 @@ using namespace std;
 
 const string initialState = "xxx_ooo";
 const string finalState = "ooo_xxx";
-set<string> Set;
 
-struct tree {   
-    string s;
-    struct tree *one, *two, *three, *four;
-    struct tree *parent;
+class Node {
+	public:
+		string s;
+		Node *parent = nullptr;
+		Node(string val, Node* par) {
+			s = val;
+			if(par == nullptr) {
+				parent = nullptr;
+			} else {
+				parent = par;
+			}
+		}	
 };
 
-struct tree *newNode(string str) {
-    struct tree *node;
-    node = (struct tree*) malloc(sizeof(struct tree));
-    node->s = str;
-    node->one = nullptr;
-    node->two = nullptr;
-    node->three = nullptr;
-    node->four = nullptr;
-    return node;
-}
-
-void display(struct tree *node) {
-    struct tree * par = node->parent;
-    while(node->parent != nullptr) {
-        cout << node->s << endl;
-        node = par;
-        par = node->parent;
-    }
-    cout << node->s << endl;
-}
 string nextState(string s, int choice) {
-    int index;
+    int index = 100;
     for(int i = 0; i < static_cast<int>(s.size()); i++) {
         if(s[i] == '_') {
             index = i;
         }
     }    
-    if(index + choice >= 0 && index + choice < 7)
+    if(index < 0 || index > 6) return "NO";
+    if(index + choice >= 0 && index + choice < 7) {
         swap(s[index], s[index + choice]);
-    return s;
-}
-
-void insertUpdated(struct tree *curr, string str, int num) {
-    switch(num) {
-        case 1: curr->one = newNode(str);
-            curr->one->parent = curr;
-            break;
-        case 2: curr->two = newNode(str);
-            curr->two->parent = curr;
-            break;
-        case 3: curr->three = newNode(str);
-            curr->three->parent = curr;
-            break;
-        case 4: curr->four = newNode(str);
-            curr->four->parent = curr;
-            break;
+    	return s;
     }
+    return "NO";
 }
 
-void generateChild(struct tree *root) {
-    queue<struct tree*> q;
-    struct tree* ans;
-    q.push(root);
-    int cnt = 0;
-    while(true) {
-        struct tree *temp = q.front();
-        q.pop();
-        cnt++;
-        if(temp->s == finalState){
-            ans = temp;
-            break;
-        }
-        string s1, s2, s3, s4, curr = temp->s;
-        s1 = nextState(curr,-2);
-        s2 = nextState(curr,-1);
-        s3 = nextState(curr,1);
-        s4 = nextState(curr,2);
-        insertUpdated(temp, s1, 1);
-        insertUpdated(temp, s2, 2);
-        insertUpdated(temp, s3, 3);
-        insertUpdated(temp, s4, 4);
-        if(Set.count(s1) == 0) {
-        	q.push(temp->one);
-        	Set.insert(s1);
-        }
-        if(Set.count(s2) == 0) {
-        	q.push(temp->two);
-        	Set.insert(s2);
-        }
-        if(Set.count(s3) == 0) {
-        	q.push(temp->three);
-        	Set.insert(s3);
-        }
-        if(Set.count(s4) == 0) {
-        	q.push(temp->four);
-        	Set.insert(s4);
-        }
-    }   
-    display(ans);
+void display(Node *node) {
+	vector<string> ans;
+	while(node != nullptr) {
+		ans.emplace_back(node->s);
+		node = node->parent;
+	}
+	reverse(ans.begin(), ans.end());
+	for(auto x: ans) cout << x << '\n';
 }
 
 int main() {
-	struct tree *root = newNode(initialState);
-    generateChild(root);
+	set<string> visited;
+	Node *node = new Node(initialState, nullptr);
+	vector<Node*> queue;
+	queue.emplace_back(node);
+	visited.insert(node->s);
+	for(int j = 0; j < static_cast<int> ((queue).size()); j++) {
+		Node *currNode = queue[j];
+		string currString = currNode->s;
+		if(currString == finalState) {
+			display(currNode);
+			return 0;
+		}
+		vector<string> states(5);
+		for(int i = 0; i < 5; i++) {
+			states[i] = nextState(currString, i - 2);
+		}
+		for(int i = 0; i < 5; i++) {
+			if(i == 2 || states[i] == "NO" || visited.count(states[i]) == 1) continue;
+			Node *child = new Node(states[i], currNode);
+			queue.emplace_back(child);
+			visited.insert(states[i]);
+		}
+	}
 	return 0;
 }
